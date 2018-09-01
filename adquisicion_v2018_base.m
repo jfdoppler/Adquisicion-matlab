a39048e04dd2a06f13c25862591073aae5d1c2e1
jfdoppler <dopplerjuan@gmail.com>
2018-09-01 12:44:56 -0300

    Implementa adquisicion en matlab usando session based interface para placas de adquisicion de NI.
    En primer lugar configura la placa: agrega los canales correspondientes,
    setea rangos de adquisicion y rate. Luego (segunda celda) corre una
    adquisicion corta para ayudar a setear los valores de triggereo. Grafica
    las dos seÃales adquiridas y las integrales de las mismas en ventanas de
    1 segundo, ademas de los espectrogramas de cada uno.
    Elegido el canal que se va a usar para triggerear se puede usar este
    grafico para ver el rango de la seÃal correspondiente en un intervalo
    sin actividad y el valor de la integral tipico en el mismo. Estos
    valores NO se ingresan en este archivo, sino en el _aux.
    
    Por ultimo (tercera celda) se corre una adquisicion en background
    (medicion) en forma continua (no bloquea la consola). En esta celda se
    debe configurar cada cuanto tiempo se llama al LISTENER. El listener es
    la funcion del archivo _aux en la que se realiza todo el
    analisis/guardado de las seÃales, el cual se debe configurar antes de
    largar cada medicion. Alli se elegira que canales se adquieren, en que
    condiciones, si se hacen playbacks, donde se guardaran los archivos,
    etc.
    
    Para detener la adquisicion se deben correr en forma consecutiva los
    comandos:
    s.stop() -> detiene la adquisicion
    delete(lh) -> borra el puntero al listener.
    
    La modificacion del listener solo se puede realizar tras correr estos
    comandos. Si no se detiene la medicion, al llamar al listener dara error
    ya que lo encontrara cambiado. Si no se lo borra, al comenzar una nueva
    adquisicion seguira apuntando al listener viejo. Ante la duda hacer un
    "daqreset" y configurar la placa nuevamente.

---
+++ adquisicion_v2018_base.m
@@ -0,0 +1,85 @@
+% Adquisicion para Matlab 64 bits Version Juan Junio 2018
+% Basada en:
+% AVIMAT Version Alan Julio 2016
+% AVIMAT Gogui versión Octubre 2014
+% AVIMAT Goldin versión Septiembre 2012
+% AVIMAT Alliende Arneodo (y el help de Matlab) versión 10-06-2010
+
+clc
+clear all
+close all
+%% Configuracion basica de la placa
+disp('Configurando...')
+daqreset
+s = daq.createSession('ni');
+% Puertos fisicos donde estan conectados los inputs
+sound_aichannel = 0;
+vs_aichannel = 1;
+% Agrego canales y seteo rate
+s_ch = addAnalogInputChannel(s, 'Dev1', sound_aichannel, 'Voltage');
+vs_ch = addAnalogInputChannel(s, 'Dev1', vs_aichannel, 'Voltage');
+vs_ch.Range = [-1, 1];
+s.Rate = 44150;
+% De los canales conectados cual es cada uno. Cuidado, importa el orden
+% P. ej si se agregaron los canales 0, 2, 1 (en ese orden)
+% x_channel = 1 == x esta conectada a la entrada 0 (ai0)
+% y_channel = 3 == y esta conectada a la entrada 1 (ai1)
+sound_channel = 1; 
+vs_channel = 2;
+disp('Listo')
+%% Setear trigger
+clc
+close all
+s.IsContinuous = false;
+s.DurationInSeconds = 2;
+% Que canal voy a mirar para usar luego como trigger
+dt_integral = 1;    % Tiempo de integracion
+samples_integral = floor(s.Rate*dt_integral);
+% Empiezo la medicion
+[data, time] = s.startForeground();
+sound = data(:,sound_channel);
+vs = data(:,vs_channel);
+subplot(6,1,1)
+plot(time, sound)
+h = subplot(6,1,2);
+[~,F,T,P]=spectrogram(sound,gausswin(round((15E-3)*s.Rate),2),...
+    round(0.97*(10E-3)*s.Rate),2^nextpow2((10E-3)*s.Rate),...
+    s.Rate,'yaxis');
+imagesc(T,F/1000,10*log10(P/20));
+set(gca,'YDir','normal');
+set(h,'YLim',[0 8]);
+subplot(6,1,3)
+plot(time, movsum(abs(sound), samples_integral))
+subplot(6,1,4)
+plot(time,vs)
+h = subplot(6,1,5);
+[~,F,T,P]=spectrogram(vs,gausswin(round((15E-3)*s.Rate),2),...
+    round(0.97*(10E-3)*s.Rate),2^nextpow2((10E-3)*s.Rate),...
+    s.Rate,'yaxis');
+imagesc(T,F/1000,10*log10(P/20));
+set(gca,'YDir','normal');
+set(h,'YLim',[0 8]);
+subplot(6,1,6)
+plot(time, movsum(abs(vs), samples_integral))
+
+%% Medicion
+clc
+close all
+% Creo un listener al que va a llamar (y ejecutar) la funcion que le digo
+% cuando tenga datos disponibles
+lh = addlistener(s, 'DataAvailable', @adquisicion_v2018_aux_dev);
+% La función está en un .m aparte. No puede ser modificada durante la 
+% medición
+
+% Adquiero en forma continua. La condicion de trigger, guardar y terminar
+% medicion se incorporan dentro del listener
+s.IsContinuous = true;
+% Defino cada cuanto tiempo llamo al listener
+% Esta variable debe estar definida igual en la funcion del listener
+dt_trigger = 1;
+s.NotifyWhenDataAvailableExceeds = floor(s.Rate*dt_trigger);
+% Empiezo la medicion
+s.startBackground();
+% PARA DETENER LA MEDICION %
+% s.stop()
+% delete(lh)
